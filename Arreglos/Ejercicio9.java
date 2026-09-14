0f628a8b354d315204c3b14eb21ce33f0e19c7dd
HALAN DAVID GONZALEZ GONZALEZ <hdavidgonzalez@estudiante.uniajc.edu.co>
2026-09-14 06:21:12 +0000

    imprime el arreglo y pide que elijas un numero entero
    esta vez si puse las librerias de primero

---
+++ Arreglos/Ejercicio9.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+        int[] A = new int[10];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = r.nextInt(10) + 1;
+    }
+    System.out.println("Arreglo A: ");
+    for (int i = 0; i < A.length; i++) {
+        System.out.print(A[i] + " ");
+    }
+    System.out.println("\nIngrese un número entero a buscar en el arreglo: ");
+    int num = sc.nextInt();
+    
+    
+    
+}
+}
