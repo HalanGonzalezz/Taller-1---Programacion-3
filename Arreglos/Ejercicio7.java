19e965b4bd18c8f90d8521d2a20abfac2bdb8f7f
HALAN DAVID GONZALEZ GONZALEZ <hdavidgonzalez@estudiante.uniajc.edu.co>
2026-09-14 05:43:47 +0000

    esto imprime el arreglo y separa entre pares he impares
    aqui la verdad si me ayudo el mismo visual a poner eso

--- Arreglos/Ejercicio7.java
+++ Arreglos/Ejercicio7.java
@@ -18,6 +18,30 @@ public class Ejercicio7 {
         }
         int[] B = new int[pares];
         int[] C = new int[impares];
-       
+
+       int indexB = 0;
+        int indexC = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] % 2 == 0) {
+                B[indexB] = A[i];
+                indexB++;
+            } else {
+                C[indexC] = A[i];
+                indexC++;
+            }
+        }
+
+        System.out.println("Arreglo A: ");
+        for (int i = 0; i < A.length; i++) {
+            System.out.print(A[i] + " ");
+        }
+        System.out.println("\nArreglo B (pares): ");
+        for (int i = 0; i < B.length; i++) {
+            System.out.print(B[i] + " ");
+        }
+        System.out.println("\nArreglo C (impares): ");
+        for (int i = 0; i < C.length; i++) {
+            System.out.print(C[i] + " ");
+        }
     }
 }
