cd2d81cfefcaab5ad8f8c0fd1802d8bf378abf4d
HALAN DAVID GONZALEZ GONZALEZ <hdavidgonzalez@estudiante.uniajc.edu.co>
2026-09-14 06:31:49 +0000

    .

---
+++ Arreglos/Ejercicio10.java
@@ -0,0 +1,13 @@
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        int[] A = {8, 5, 3, 10, 2, 8, 1};
+        int n = A.length;
+
+        int tamB = (n % 2 == 0) ? n / 2 : n / 2 + 1;
+        int[] B = new int[tamB];
+
+        
+        }
+    }
+    
+
