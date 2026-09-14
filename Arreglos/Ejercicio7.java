fea73efeffe502b59bf3cfc4d40fa11c583c87ac
HALAN DAVID GONZALEZ GONZALEZ <hdavidgonzalez@estudiante.uniajc.edu.co>
2026-09-14 05:23:08 +0000

    esto llena el arreglo y dece cuantos son impares y pares

--- Arreglos/Ejercicio7.java
+++ Arreglos/Ejercicio7.java
@@ -5,6 +5,18 @@ public class Ejercicio7 {
 
         int[] A = new int[10];
         Random r = new Random();
-        System.out.println(A.length);
+        
+        int pares = 0;
+        int impares = 0;
+        for (int i = 0; i < A.length; i++) {
+            A[i] = r.nextInt(50) + 1;
+            if (A[i] % 2 == 0) {
+                pares++;
+            } else {
+                impares++;
+            }
+        }
+        System.out.println("Pares: " + pares);
+        System.out.println("Impares: " + impares);
     }
 }
