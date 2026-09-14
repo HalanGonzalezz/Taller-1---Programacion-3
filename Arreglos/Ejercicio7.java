496371e7572c2ecdcfaa3e4fc4de0cb70ba86836
HALAN DAVID GONZALEZ GONZALEZ <hdavidgonzalez@estudiante.uniajc.edu.co>
2026-09-14 05:10:53 +0000

    declare el arreglo de 10 elementos y lo de generar numeros aleatorios
    tambien probe lo de los arreglos (se me olvido un poco)

---
+++ Arreglos/Ejercicio7.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+
+        int[] A = new int[10];
+        Random r = new Random();
+        System.out.println(A.length);
+    }
+}
