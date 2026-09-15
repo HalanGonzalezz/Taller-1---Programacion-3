d7d1ab74ed2a7c56f8ce3350429899edd30e0365
HALAN DAVID GONZALEZ GONZALEZ <hdavidgonzalez@estudiante.uniajc.edu.co>
2026-09-15 04:26:53 +0000

    hice que sumara las filas y columnas y lo imprimiera (con ayuda del mismo visual)

--- Matrices/Ejercicio17.java
+++ Matrices/Ejercicio17.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class Ejercicio17 { 
+public class Ejercicio17 {
     public static void main(String[] args) {
         int filas = 3;
         int columnas = 3;
@@ -11,11 +11,26 @@ public class Ejercicio17 {
             for (int j = 0; j < columnas; j++) {
                 M[i][j] = r.nextInt(10) + 1;
 
-                    System.out.print(M[i][j] + " ");
+                System.out.print(M[i][j] + " ");
             }
-           System.out.println();
+            System.out.println();
         }
-        
+        for (int i = 0; i < filas; i++) {
+            int sumaFila = 0;
+            for (int j = 0; j < columnas; j++) {
+                sumaFila += M[i][j];
+            }
+            System.out.println("La suma de la fila " + i + " es: " + sumaFila);
+
+        }
+        for (int j = 0; j < columnas; j++) {
+            int sumaColumna = 0;
+            for (int i = 0; i < filas; i++) {
+                sumaColumna += M[i][j];
+            }
+            System.out.println("La suma de la columna " + j + " es: " + sumaColumna);
+
+        }
     }
-    
+
 }
